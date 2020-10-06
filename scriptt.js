818b7eab471af6ed48f441293d540b8dfa1eca4c
Evgenix2001 <70563042+Evgenix2001@users.noreply.github.com>
2020-10-06 10:05:18 +0300

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,25 +1,53 @@
+const num = document.getElementById("number");
 const btn1 = document.getElementById("increment");
 const btn2 = document.getElementById("decrement");
 const btn3 = document.getElementById("reset");
-const num = document.getElementById("number");
 const btn4 = document.getElementById("Red");
 const btn5 = document.getElementById("Green");
 const btn6 = document.getElementById("Blue");
-btn1.addEventListener("click", () => {
+const btn7 = document.getElementById("result");
+ btn1.addEventListener("click", () => {
   num.innerHTML++;
 });
-btn2.addEventListener("click", () => {
+ btn2.addEventListener("click", () => {
   num.innerHTML--;
 });
-btn3.addEventListener("click", () => {
+ btn3.addEventListener("click", () => {
   num.innerHTML= "1";
 });
-btn4.addEventListener("click", () => {
+ btn4.addEventListener("click", () => {
   num.style="color:red;" ;
 });
-btn5.addEventListener('click', () => {
+ btn5.addEventListener('click', () => {
   num.style="color:green;" ;
 });
-btn6.addEventListener('click', () => {
+ btn6.addEventListener('click', () => {
   num.style="color:blue;" ;
 });
+ btn7.addEventListener('click', () => {
+   num.innerHTML= "result" ;
+   });
+
+  function func() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    switch (op) {
+      case 'plus':
+        result = a + b;
+        break;
+      case 'minus':
+        result = a - b;
+        break;
+      case 'times':
+        result = a * b;
+        break;
+      case 'divide':
+        if (num2) {
+          result = a / b;
+        } else {
+          result = 'mm';
+        }
+        break;
+    }
+    document.getElementById("resultat").innerHTML = result;
+  }
